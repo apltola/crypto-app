d250653864f03752a46d801243aac15c9cbdb10e
allu <aleksijaakkojuhani@gmail.com>
2020-10-15 12:33:07 +0300

    adding client

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('jeejee crypto_app');
+app.get('/api/test', (req, res) => {
+  res.send('jeejee crypto app testing testing');
 });
+
+if (process.env.NODE_ENV === 'production') {
+  const dist = path.resolve(__dirname, 'client', 'dist');
+  app.use('/', express.static(dist));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(dist, 'index.html'));
+  });
+}
 
 app.listen(3000, () => {
   console.log('server up at 3000');
