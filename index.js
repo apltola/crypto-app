4351774caf5e694fecd27041c541308b599d93ec
allu <aleksijaakkojuhani@gmail.com>
2020-10-15 13:47:22 +0300

    production test

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ app.get('/api/test', (req, res) => {
   );
 });
 
-if (process.env.NODE_ENV === 'production') {
-  console.log('production!!');
-  const dist = path.resolve(__dirname, 'client', 'dist');
-  app.use('/', express.static(dist));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(dist, 'index.html'));
-    //res.sendFile(path.resolve(__dirname, 'testi.html'));
-  });
-}
+console.log('production!!');
+const dist = path.resolve(__dirname, 'client', 'dist');
+app.use('/', express.static(dist));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(dist, 'index.html'));
+  //res.sendFile(path.resolve(__dirname, 'testi.html'));
+});
 
 app.listen(3000, () => {
   console.log('server up at 3000');
