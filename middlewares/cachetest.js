1c51777a9d83fc6bee26b53b211842aa99d395bf
allu <aleksijaakkojuhani@gmail.com>
2020-10-17 13:05:27 +0300

    no cache

--- middlewares/cachetest.js
+++ middlewares/cachetest.js
@@ -2,7 +2,7 @@ module.exports = function (req, res, next) {
   console.log('req.url -> ', req.url);
   if (req.url.includes('.css') || req.url.includes('.js') || req.url === '/') {
     console.log('css or javascript or root');
-    res.setHeader('Cache-Control', `max-age=0, no-cache`);
+    res.setHeader('Cache-Control', `no-store`);
   }
 
   next();
